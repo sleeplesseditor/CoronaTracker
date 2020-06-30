02f59df7347522964fac8c4fed075c56df0076dc
Josh Bryson <josh@bryson.me.uk>
2020-06-30 17:07:24 +0100

    View option tabs implemented

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import styles from './App.module.css';
 import { Cards, Chart, CountryPicker } from './components';
+import { Tab, Tabs } from '@material-ui/core';
 import globleImage from './img/globe.png';
 import { fetchData, fetchMapData } from './api';
 
 class App extends Component {
   state = {
     data: {},
-    country: ''
+    mapData: {},
+    country: '',
+    countryView: 0
   }
 
   async componentDidMount() {
     const fetchedData = await fetchData();
-    const mapData = await fetchMapData();
-    console.log('TODAY', mapData)
+    const mapTagData = await fetchMapData();
 
-    this.setState({ data: fetchedData })
+    this.setState({ 
+      data: fetchedData,
+      mapData: mapTagData
+    })
   }
 
   handleCountryChange = async (country) => {
@@ -23,8 +28,32 @@ class App extends Component {
     this.setState({ data: fetchedData, country: country });
   }
 
+  handleTabChange = (event, newValue) => {
+    this.setState({
+      countryView: newValue
+    })
+  }
+
+  renderChartView(data, country) {
+    return (
+      <Fragment>
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+      </Fragment>
+    )
+  }
+
+  renderMapView() {
+    return (
+      <div>
+        MAP
+      </div>
+    )
+  }
+
+
   render() {
-    const { country, data } = this.state;
+    const { country, countryView, data  } = this.state;
 
     return (
       <div className={styles.container}>
@@ -33,8 +62,22 @@ class App extends Component {
           <h1 className={styles.title}>COVID-19 Tracker</h1>
         </div>
         <Cards data={data} />
-        <CountryPicker handleCountryChange={this.handleCountryChange} />
-        <Chart data={data} country={country} />
+        <Tabs
+          value={countryView}
+          onChange={this.handleTabChange}
+          indicatorColor="primary"
+          textColor="primary"
+          centered
+        >
+          <Tab label="Chart View" />
+          <Tab label="Map View" />
+        </Tabs>
+        {countryView === 0 ? (
+          <CountryPicker handleCountryChange={this.handleCountryChange} />
+        ): null}
+        {countryView === 0 ? (
+          <Chart data={data} country={country} />
+        ): this.renderMapView()}
       </div>
     )
   }
